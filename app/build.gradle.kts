16d448221f5e4aac3c29cc4ab7731903c472c87e
MRSa <mrsa@myad.jp>
2025-05-11 00:20:07 +0900

    使用ライブラリのバージョン更新。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "jp.osdn.gokigen.tellomove"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10001
+        versionName = "1.0.1"
     }
 
     buildTypes {
